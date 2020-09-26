d651d1ed8dce792d67aee75a9e70fe6c0476f39d
Caio Hudson <caiohudson.red4@hotmail.com>
2020-09-26 10:54:09 -0300

    adicionando ao git

---
+++ Aulas/01-ClienteServidor/Cliente/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Cliente
+{
+    class Program
+    {
+        const int porta = 7000;
+        const string ip_servidor = "127.0.0.1";
+        static void Main(string[] args)
+        {
+            TcpClient cliente = new TcpClient();
+            Console.WriteLine("Conectando ao servidor.....");
+            try
+            {
+                cliente.Connect(ip_servidor,porta);
+                Console.WriteLine("OK!");
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Falhou!");
+
+            }
+        }
+    }
+}
---
+++ Aulas/01-ClienteServidor/Servidor/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Servidor
+{
+    class Program
+    {
+        //Objetos para criar uma porta
+        object madeira, maçaneta, furadeira, martelo, pregos, tranca;
+        const int porta = 7000;
+        static void Main(string[] args)
+        {
+            TcpListener servidor = new TcpListener(IPAddress.Any,porta);
+            try
+            {
+                Console.WriteLine("Servidor no ar");
+                servidor.Start();
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Você é um fracasso");
+            }
+
+            while(true){
+                TcpClient cliente = servidor.AcceptTcpClient();
+                Console.WriteLine("Conexão estabelecida");
+
+                cliente.Close();
+                Console.WriteLine("Conexão encerrada!");
+            }
+        }
+    }
+}
