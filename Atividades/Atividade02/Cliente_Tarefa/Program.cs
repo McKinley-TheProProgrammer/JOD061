0875eeaaf8211f60a2fa89f08ae861dfa05adc4b
McKinley-TheProProgrammer <McKinleyTheProProgrammer@users.noreply.github.com>
2020-10-09 09:28:42 -0300

    com

---
+++ Atividades/Atividade02/Cliente_Tarefa/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Cliente_Tarefa
+{
+    class Program
+    {
+        const int porta = 7000;
+        const string ip_servidor = "127.0.0.1";
+        static void Main(string[] args)
+        {
+            TcpClient cliente = new TcpClient();
+            Console.WriteLine("Conectando ao servidor.....");
+            try
+            {
+                cliente.Connect(ip_servidor,porta);
+                Console.WriteLine("OK!");
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Falhou!");
+
+            }
+            byte[] bytes = new byte[1024];
+            NetworkStream stream = cliente.GetStream();
+
+            Console.WriteLine("Envie uma msg. P/ sair, pressione ENTER");
+            Console.WriteLine(">");
+
+            string msg = Console.ReadLine();
+
+            while(msg.Length < 0){
+                bytes = Encoding.ASCII.GetBytes();
+                stream.Write(bytes,0,bytes.Length);
+
+                Console.Write(">");
+                msg = Console.ReadLine();
+            }
+            stream.Close();
+            Console.WriteLine("Desconectado!");
+            cliente.Close();
+        }
+    }
+}
