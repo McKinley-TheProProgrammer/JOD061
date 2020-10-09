d674f358684da15ef5c8428248c534313298cbeb
McKinley-TheProProgrammer <McKinleyTheProProgrammer@users.noreply.github.com>
2020-10-09 14:24:03 -0300

    Atividades Enviadas

---
+++ Atividades/Atividade02/Servidor_tarefa/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Servidor_Tarefa
+{
+    class Program
+    {
+        //Objetos para criar uma porta
+        object madeira, maçaneta, furadeira, martelo, pregos, tranca;
+        const int porta = 7000;
+        static void Main(string[] args)
+        {
+            TcpListener servidor = new TcpListener(IPAddress.Any,porta);
+            try
+            {
+                Console.WriteLine("Servidor no ar");
+                servidor.Start();
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Você é um fracasso");
+            }
+          
+            while(true){
+                TcpClient cliente = servidor.AcceptTcpClient();
+                Console.WriteLine("Conexão estabelecida");
+
+                byte[] bytes = new byte[1024];
+                NetworkStream stream = cliente.GetStream();
+
+                while(true)
+                {
+                    int bytesRead = stream.Read(bytes, 0, bytes.Length);
+
+                    if(bytesRead == 0)
+                    {
+                        break;
+                    }
+                    Console.WriteLine("Mensagem Recebida: {0}",Encoding.ASCII.GetString(bytes, 0,bytes.Length));
+                }
+                cliente.Close();
+                Console.WriteLine("Conexão encerrada!");
+            }
+        }
+    }
+}
---
+++ Atividades/Atividade03/Udp_tarefa/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace _02_Ponto2Ponto_Tarefa
+{
+    class Program
+    {
+        const int porta = 7000;
+
+        const string ip = "10.0.2.255";
+        static void Main(string[] args)
+        {
+            try{
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Any,0);
+            UdpClient par = new UdpClient(porta);
+            Console.WriteLine("Para sair, pressione CTRL+C");
+
+            while(true)
+            {
+                byte[] bytesRecebidos = par.Receive(ref endPoint);
+                Console.WriteLine("Mensagem recebida: {0}", Encoding.ASCII.GetString(bytesRecebidos));
+                Console.WriteLine("Mensagem enviada por {0} : {1}",endPoint.Address.ToString(),endPoint.Port.ToString());
+            }
+            } 
+            catch(Exception){
+
+                UdpClient par = new UdpClient();
+                par.EnableBroadcast = true;
+                Console.WriteLine("Envie uma mensagem. Pressione ENTER para sair");
+                Console.WriteLine("<");
+                string msg = Console.ReadLine();
+
+                while(msg.Length > 0)
+                {
+                    byte[] bytesEnviados = Encoding.ASCII.GetBytes(msg);
+                    par.Send(bytesEnviados,bytesEnviados.Length,ip,porta);
+
+                    Console.Write(">");
+                    msg = Console.ReadLine();
+
+                }
+                par.Close();
+            }
+        }
+    }
+}
