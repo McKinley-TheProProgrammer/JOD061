21047bb87dc19a42a6c577cd2a43c4d5680e83ff
McKinley-TheProProgrammer <McKinleyTheProProgrammer@users.noreply.github.com>
2020-12-04 03:09:30 -0300

    Update 04 12

--- Atividades/Atividade06/Servidor da Dor/Assets/Scripts/GameManager.cs
+++ Atividades/Atividade06/Servidor da Dor/Assets/Scripts/GameManager.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
+using TMPro;
 
 public class GameManager : MonoBehaviourPunCallbacks
 {
     [SerializeField] Transform[] spawnFields;
+    [SerializeField] float countdown = 3;
+    PlayerController scriptToDeactivate;
+    [SerializeField] TextMeshProUGUI countdownText;
     void Start()
     {
         if (!PhotonNetwork.IsConnected)
@@ -14,14 +18,30 @@ public class GameManager : MonoBehaviourPunCallbacks
             PhotonNetwork.LoadLevel(0);
             return;
         }
-        PhotonNetwork.Instantiate("ServerPlayer", spawnFields[Random.Range(0,spawnFields.Length)].position, Quaternion.identity, 0);
-        
-
+        GameObject playerController = PhotonNetwork.Instantiate("ServerPlayer", spawnFields[Random.Range(0,spawnFields.Length)].position, Quaternion.identity, 0);
+        scriptToDeactivate = playerController.GetComponent<PlayerController>();
+        scriptToDeactivate.enabled = false;
     }
 
+    void CountingDown()
+    {
+        countdown -= Time.deltaTime;
+        int someInt = (int)countdown;
+        countdownText.text = someInt.ToString();
+        if(countdown <= 0)
+        {
+            scriptToDeactivate.enabled = true;
+            countdown = 0;
+            countdownText.gameObject.SetActive(false);
+        }
+    }
     // Update is called once per frame
     void Update()
     {
+        if (PhotonNetwork.IsConnected)
+        {
+            CountingDown();
+        }
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             PhotonNetwork.LeaveRoom();
